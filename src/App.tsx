08ee64f5ab3721dc5c79f2761ec37121c4976169
Grayson <glb002@mcdaniel.edu>
2023-09-24 20:04:56 -0400

    Use off-white, add board container.

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Board
-          difficulty={diff}
-          isGameStarted={isGameStarted}
-        />
-        <div className='game-options'>
-          {isGameStarted &&
-            <button className='btnPrimary' onClick={() => { setIsGameStarted(false) }}>Quit</button>
-          }
-          {!isGameStarted &&
-            <>
-              <div className='select-group'>
-                <span className='select-label'>Difficulty:</span>
-                <select className='select-difficulty' onChange={(e) => { setDiff(e.target.value as Difficulty); }}>
-                  <option value={Difficulty.Easy}>{Difficulty.Easy}</option>
-                  <option value={Difficulty.Medium}>{Difficulty.Medium}</option>
-                  <option value={Difficulty.Hard}>{Difficulty.Hard}</option>
-                </select>
-              </div>
-              <button className='btnPrimary' onClick={() => { setIsGameStarted(true) }}>Start</button>
-            </>
-          }
+        <div className='board-container'>
+          <Board
+            difficulty={diff}
+            isGameStarted={isGameStarted}
+          />
+          <div className='game-options'>
+            {isGameStarted &&
+              <button className='btnPrimary' onClick={() => { setIsGameStarted(false) }}>Quit</button>
+            }
+            {!isGameStarted &&
+              <>
+                <div className='select-group'>
+                  <span className='select-label'>Difficulty:</span>
+                  <select className='select-difficulty' onChange={(e) => { setDiff(e.target.value as Difficulty); }}>
+                    <option value={Difficulty.Easy}>{Difficulty.Easy}</option>
+                    <option value={Difficulty.Medium}>{Difficulty.Medium}</option>
+                    <option value={Difficulty.Hard}>{Difficulty.Hard}</option>
+                  </select>
+                </div>
+                <button className='btnPrimary' onClick={() => { setIsGameStarted(true) }}>Start</button>
+              </>
+            }
+          </div>
         </div>
       </header>
     </div>
