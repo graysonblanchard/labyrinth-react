529a1c45ea3b23a9c44ae6f2a34a33d64ed2bbeb
Grayson <glb002@mcdaniel.edu>
2023-09-28 17:48:39 -0400

    Disabled med & hard again.

--- src/App.tsx
+++ src/App.tsx
@@ -164,8 +164,8 @@ function App() {
                   <span className='select-label'>Difficulty:</span>
                   <select className='select-difficulty' value={diff} onChange={(e) => { setDiff(e.target.value as Difficulty); }}>
                     <option value={Difficulty.Easy}>Easy</option>
-                    <option value={Difficulty.Medium}>Medium</option>
-                    <option value={Difficulty.Hard}>Hard</option>
+                    <option disabled value={Difficulty.Medium}>Medium</option>
+                    <option disabled value={Difficulty.Hard}>Hard</option>
                   </select>
                 </div>
                 <button className='btnPrimary' onClick={() => { setIsNewGameStarted(true); }}>Start</button>
